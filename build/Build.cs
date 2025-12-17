a416137738f200e4de456c44df0c6b6114949003
vlastroG <vlastroganov@gmail.com>
2025-12-17 11:36:40 +0300

    Добавлена сборка через nuke

---
+++ build/Build.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using dosymep.Nuke.RevitVersions;
+
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Components;
+
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild, IHazSolution {
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    /// <summary>
+    ///     Max Revit version.
+    /// </summary>
+    [Parameter("Max Revit version.")]
+    readonly RevitVersion MaxVersion = RevitVersion.Rv2024;
+
+    /// <summary>
+    ///     Min Revit version.
+    /// </summary>
+    [Parameter("Min Revit version.")]
+    readonly RevitVersion MinVersion = RevitVersion.Rv2020;
+
+    [Parameter]
+    readonly AbsolutePath Output = RootDirectory / "bin";
+
+    [Parameter("Build Revit versions.")]
+    readonly RevitVersion[] RevitVersions = new RevitVersion[0];
+
+    IEnumerable<RevitVersion> BuildRevitVersions;
+
+    Target Clean =>
+        _ => _
+            .Before(Restore)
+            .Executes(() => {
+                Output.CreateOrCleanDirectory();
+                RootDirectory.GlobDirectories("**/bin", "**/obj")
+                    .Where(item => item != RootDirectory / "build" / "bin")
+                    .Where(item => item != RootDirectory / "build" / "obj")
+                    .DeleteDirectories();
+            });
+
+    Target Restore =>
+        _ => _
+            .DependsOn(Clean)
+            .Executes(() => {
+                DotNetRestore(s => s
+                    .SetProjectFile(((IHazSolution) this).Solution));
+            });
+
+    Target Compile =>
+        _ => _
+            .DependsOn(Restore)
+            .Executes(() => {
+                DotNetBuild(s => s
+                    .EnableForce()
+                    .DisableNoRestore()
+                    .SetConfiguration(Configuration)
+                    .SetProjectFile(((IHazSolution) this).Solution)
+                    .CombineWith(
+                        BuildRevitVersions,
+                        (settings, version) => {
+                            return settings
+                                .SetOutputDirectory(Output / version)
+                                .SetProperty("RevitVersion", (int) version);
+                        }));
+            });
+
+    protected override void OnBuildInitialized() {
+        base.OnBuildInitialized();
+        BuildRevitVersions = RevitVersions.Length > 0
+            ? RevitVersions
+            : RevitVersion.GetRevitVersions(MinVersion, MaxVersion);
+    }
+
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
+}
