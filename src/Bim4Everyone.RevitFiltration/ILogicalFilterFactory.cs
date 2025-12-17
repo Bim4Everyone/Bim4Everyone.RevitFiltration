e9e8db82eef8702e71ea8a5de96e8a7456163abc
vlastroG <vlastroganov@gmail.com>
2025-12-17 18:17:57 +0300

    Добавить IOptions в параметр методов

--- src/Bim4Everyone.RevitFiltration/ILogicalFilterFactory.cs
+++ src/Bim4Everyone.RevitFiltration/ILogicalFilterFactory.cs
@@ -7,12 +7,14 @@ public interface ILogicalFilterFactory {
     /// <summary>
     ///     Создает фильтр с условием "И".
     /// </summary>
+    /// <param name="options">Настройки генерации правил фильтрации параметров.</param>
     /// <returns>Ссылка на созданный фильтр.</returns>
-    ILogicalFilter CreateAndFilter();
+    ILogicalFilter CreateAndFilter(IOptions options);
 
     /// <summary>
     ///     Создает фильтр с условием "Или".
     /// </summary>
+    /// <param name="options">Настройки генерации правил фильтрации параметров.</param>
     /// <returns>Ссылка на созданный фильтр.</returns>
-    ILogicalFilter CreateOrFilter();
+    ILogicalFilter CreateOrFilter(IOptions options);
 }
