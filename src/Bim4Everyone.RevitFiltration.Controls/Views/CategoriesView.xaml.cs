19ef6c437f2048d316466a069e68369e68eabc70
sssenya <senia7@bk.ru>
2026-02-03 17:00:36 +0300

    Исправлена проблема, из-за которой не отображается текст Header в DataGridTextColumn

--- src/Bim4Everyone.RevitFiltration.Controls/Views/CategoriesView.xaml.cs
+++ src/Bim4Everyone.RevitFiltration.Controls/Views/CategoriesView.xaml.cs
@@ -1,9 +1,19 @@
+using System.Windows;
 using System.Windows.Controls;
+
+using Bim4Everyone.RevitFiltration.Controls.ViewModels;
 
 namespace Bim4Everyone.RevitFiltration.Controls.Views;
 
 internal partial class CategoriesView : UserControl {
     public CategoriesView() {
         InitializeComponent();
+        this.DataContextChanged += OnDataContextChanged;
+    }
+    
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+        if(e.NewValue is DynamicCategoriesFilterViewModel vm) {
+            this.Resources["NameHeader"] = vm.NameHeader;
+        }
     }
 }
