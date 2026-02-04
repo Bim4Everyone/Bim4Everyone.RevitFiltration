dde901582430476e8c56a90190a6175c3125bf91
vlastroG <vlastroganov@gmail.com>
2026-02-04 15:57:01 +0300

    Удален ненужный код

--- src/Bim4Everyone.RevitFiltration.Controls/Models/FilterModel/CompositorKind.cs
+++ src/Bim4Everyone.RevitFiltration.Controls/Models/FilterModel/CompositorKind.cs
@@ -1,11 +1,6 @@
-using System.ComponentModel;
-
 namespace Bim4Everyone.RevitFiltration.Controls.Models.FilterModel;
 
 internal enum CompositorKind {
-    [Description("И")]
     And,
-
-    [Description("Или")]
     Or
 }
--- src/Bim4Everyone.RevitFiltration.Controls/Models/FilterModel/OperatorKind.cs
+++ src/Bim4Everyone.RevitFiltration.Controls/Models/FilterModel/OperatorKind.cs
@@ -1,47 +1,18 @@
-using System.ComponentModel;
-
 namespace Bim4Everyone.RevitFiltration.Controls.Models.FilterModel;
 
 internal enum OperatorKind {
-    [Description("Равно")]
     Equals,
-
-    [Description("Не равно")]
     NotEquals,
-
-    [Description("Имеет значение")]
     HasValue,
-
-    [Description("Без значения")]
     HasNoValue,
-
-    [Description("Больше")]
     Greater,
-
-    [Description("Больше или равно")]
     GreaterOrEqual,
-
-    [Description("Меньше")]
     Less,
-
-    [Description("Меньше или равно")]
     LessOrEqual,
-
-    [Description("Начинается с")]
     BeginsWith,
-
-    [Description("Не начинается с")]
     NotBeginsWith,
-
-    [Description("Содержит")]
     Contains,
-
-    [Description("Не содержит")]
     NotContains,
-
-    [Description("Заканчивается на")]
     EndsWith,
-
-    [Description("Не заканчивается на")]
     NotEndsWith
 }
